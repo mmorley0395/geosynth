c266a3693b6a1a6c73b4350d48a72b27895b1152
mmorley <mmorley0395@gmail.com>
2022-02-19 13:58:00 -0500

    mouse over working!

--- main.js
+++ main.js
@@ -63,19 +63,28 @@ map.on("load", () => {
 });
 
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
+var ac = new window.AudioContext() || new window.webkitAudioContext();
+var osc;
 // create Oscillator node
+function playosc() {
+  osc = ac.createOscillator();
+  osc.type = "sine";
+  osc.frequency.value = 340; // value in hertz
+  osc.connect(ac.destination);
+  osc.start(0.5);
+  console.log("hello?");
+}
+
+function stoposc() {
+  osc.stop();
+}
 
 map.on("mouseenter", "amtrak", (e) => {
-  const oscillator = audioCtx.createOscillator();
-  oscillator.type = "sine";
-  oscillator.frequency.setValueAtTime(340, audioCtx.currentTime); // value in hertz
-  oscillator.connect(audioCtx.destination);
-  oscillator.start(0);
+  playosc();
 });
 
 map.on("mouseleave", "amtrak", (e) => {
-  oscillator.stop(0);
+  stoposc();
 });
 
 // create web audio api context
